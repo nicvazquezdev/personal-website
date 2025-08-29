a96a584814133612a477147f4e8e32618d10d79d
Nicolas Vazquez <nicorvazquezs@gmail.com>
2025-08-29 19:10:20 -0300

    Refactor InfoSection component: simplify active button styling by removing underline offset and adjust link display properties for improved consistency and clarity.

--- app/components/InfoSection.tsx
+++ app/components/InfoSection.tsx
@@ -25,7 +25,7 @@ export default function InfoSection({ data }: InfoSectionProps) {
             onClick={() => handleButtonClick(key)}
             className={`hover:text-white text-lg md:text-base cursor-pointer ${
               activeInfo === key
-                ? "text-white decoration-gray-400 underline underline-offset-4"
+                ? "text-white decoration-gray-400"
                 : "text-gray-400 hover:decoration-gray-500"
             }`}
           >
@@ -53,7 +53,7 @@ export default function InfoSection({ data }: InfoSectionProps) {
                         ? "noopener noreferrer"
                         : undefined
                     }
-                    className="text-lg block w-full hover:text-white underline underline-offset-4"
+                    className="text-lg md:text-base inline-block hover:text-white underline  underline-offset-4 hover:underline"
                   >
                     <span className="relative">
                       {link.name}
