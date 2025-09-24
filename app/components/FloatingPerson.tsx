b42880857757374319ef3c7b30a0e3d7c45b80db
Nicolas Vazquez <nicorvazquezs@gmail.com>
2025-09-23 22:13:48 -0300

    update FloatingPerson component: introduce delay in mouse up event to enhance user interaction experience

--- app/components/FloatingPerson.tsx
+++ app/components/FloatingPerson.tsx
@@ -13,7 +13,11 @@ export default function FloatingPerson() {
         }
         alt="Pixel art of a person lying down, floating in nothingness"
         onMouseDown={() => setIsClicked(true)}
-        onMouseUp={() => setIsClicked(false)}
+        onMouseUp={() => {
+          setTimeout(() => {
+            setIsClicked(false);
+          }, 1000);
+        }}
         className="hidden md:block absolute right-0 top-40 md:top-80 md:right-120 transform scale-x-[-1] opacity-50 animate-float cursor-pointer"
         width={180}
         height={180}
