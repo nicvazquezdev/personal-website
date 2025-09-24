9c9542c94284ef94e727a6abaafc0b81e40817d9
Nicolas Vazquez <nicorvazquezs@gmail.com>
2025-09-24 16:43:54 -0300

    refactor FloatingPerson component: replace image assets with new avatar images, implement timeout for click state reset, and remove unnecessary mouse event handlers for improved performance

--- app/components/FloatingPerson.tsx
+++ app/components/FloatingPerson.tsx
@@ -1,27 +1,30 @@
 "use client";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function FloatingPerson() {
   const [isClicked, setIsClicked] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsClicked(false);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, [isClicked]);
 
   return (
     <>
       <Image
         src={
-          isClicked ? "/floating-person-clicked.png" : "/floating-person.png"
+          isClicked ? "/floating-avatar-clicked.png" : "/floating-avatar.png"
         }
         alt="Pixel art of a person lying down, floating in nothingness"
         onMouseDown={() => setIsClicked(true)}
-        onMouseUp={() => {
-          setTimeout(() => {
-            setIsClicked(false);
-          }, 1000);
-        }}
         className="hidden md:block absolute right-0 top-40 md:top-80 md:right-120 transform scale-x-[-1] opacity-50 animate-float cursor-pointer"
         width={180}
         height={180}
         priority
+        draggable={false}
       />
     </>
   );
